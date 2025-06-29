f9ec6cec45a2e90c3a2cb67c6f9745bf05ba7a67
Pratik Pahire <pahire288@gmail.com>
2025-06-29 17:55:37 +0530

    Added table.js problem solution

---
+++ table.js
@@ -0,0 +1,13 @@
+for(let i=2;i<5;i++)
+{
+    console.log("Below table is of",i)
+    let table = i;
+    console.log(i)
+    
+    for(let j=1;j<10;j++)
+    {
+        table +=i;
+       console.log(table);
+    }
+    
+}
